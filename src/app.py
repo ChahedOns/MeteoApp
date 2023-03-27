63ff3eb96bf62c66c29fcbe025c25628993e43a3
ChahedOns <chahedons1@gmail.com>
2023-03-27 18:01:32 +0100

    Changes

--- src/app.py
+++ src/app.py
@@ -3,12 +3,14 @@ import requests
 from flask import Flask, make_response, request, jsonify, render_template, send_file , url_for
 from flask_mongoengine import MongoEngine
 from mongoengine import EmbeddedDocumentListField, ReferenceField, EmbeddedDocumentField, ListField
-from APIConst import db_name, user_pwd, api_key
+from APIConst import db_name, user_pwd, api_key,user_db
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired
 import json
+#from werkzeug.security import check_password_hash, generate_password_hash, safe_str_cmp
 
 # configurations !
 app = Flask(__name__)
-DB_URI = f"mongodb+srv://{user}:{user_pwd}@cluster0.ad4hkct.mongodb.net/{db_name}?retryWrites=true&w=majority"
+DB_URI = f"mongodb+srv://{user_db}:{user_pwd}@cluster0.ad4hkct.mongodb.net/{db_name}?retryWrites=true&w=majority"
 app.config["MONGODB_HOST"] = DB_URI
 app.config.update(dict(
     DEBUG=True,
@@ -23,8 +25,8 @@ db = MongoEngine()
 db.init_app(app)
 #database definition
 class user(db.Document):
-    id = db.StringField()
     mail = db.StringField(required=True)
+    pwd= db.StringField(required=True)
     name = db.StringField(required=True)
     birth_date = db.DateTimeField(required=True)
     location=db.StringField(required=True)
@@ -38,31 +40,61 @@ class user(db.Document):
         }
     
 class place(db.Document):
-    id=db.StringField()
     name=db.StringField(required=True)
     lat=db.FloatField()
     lon=db.StringField()
 
     def to_json(self):
         return{
-            "ID": self.id,
             "Name": self.name,
             "Lat":self.lat,
             "Lon":self.lon
         }
 
 class weather(db.Document):
-    id=db.StringField()
     data=db.DictField()
-    date=db.DateTimeField(default=datetime.utcnow)
+    date=db.DateField(default=datetime.datetime.now)
     city=db.StringField()
     def to_json(self):
         return{
-            "ID": self.id,
             "Data": self.data,
             "Date":self.date,
             "City":self.city
         }
+
+
+"""def authenticate(mail, password):
+    user = user.objects(mail=mail)
+    if user and safe_str_cmp(user.pwd.encode('utf-8'), password.encode('utf-8')):
+        return user  """
+
+# App Routers
+@app.route("/")
+def hello_world():
+    return "<h1> Hello, World!</h1>"
+
+
+@app.route("/cites", methods=["POST", 'GET'])
+def set_place():
+    if request.method == "POST":
+        with open('../json/cities.json', 'r') as f:
+            data = json.load(f)
+            i = 0
+            while i < len(data):
+                C = place( name=data[i]["name"],
+                            lat=float(data[i]["gps_lat"]),
+                            lon=float(data[i]["gps_lng"]))
+                C.save()
+                i = i + 1
+            return make_response("Tous les cités sont ajoutés avec succées! ", 200)
+    else:
+        Ls = []
+        for r in place.objects():
+            Ls.append(r)
+        if Ls == []:
+            return make_response("Aucun cite dans le systéme!", 201)
+        else:
+            return make_response(jsonify("tous les cites sont : ", Ls), 200)
     
 def get_weather_data(api_key, city):
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
@@ -72,37 +104,20 @@ def get_weather_data(api_key, city):
         return json.dumps(data, indent = 3)
     else:
         return None
-    
 
-# App Routers
-@app.route("/")
-def hello_world():
-    return "<h1> Hello, World!</h1>"
-
-
-@app.route("/cite", methods=["POST", 'GET'])
-def set_cite():
+@app.route("/weather", methods=["POST", 'GET'])
+def set_weather(city=None):
     if request.method == "POST":
-        with open('json/cities.json', 'r') as f:
-            data = json.load(f)
-            i = 0
-            while i < len(data):
-                D = Dept.objects(code_dept=data[i]["department_code"]).first
-                if D == None:
-                    return make_response("Departement inexistant", 201)
-                else:
-                    C = Cities(id_cite=int(data[i]["id"]), dept=data[i]["department_code"], nom_cite=data[i]["name"],
-                               lat=float(data[i]["gps_lat"]), slug_cite=data[i]["slug"],
-                               lng=float(data[i]["gps_lng"]))
-                    C.save()
-                i = i + 1
+        data= get_weather_data(api_key , city)
+        w= weather(data=data,city=city)
+        #Check if the place exist in our data base! 
+        p = place.objects(name=city).first()
+        if p == None:
+            p=place(name=city,lat=data["coord"]["lat"],lon=data["coord"]["lon"])
+            p.save()
+        
+        w.save()
+        return make_response("Ajout du météo avec succées! ", 200)
+        
+        
 
-            return make_response("Ajout de tous les départements avec succés", 200)
-    else:
-        Ls = []
-        for r in Cities.objects():
-            Ls.append(r)
-        if Ls == []:
-            return make_response("Aucun cite dans le systéme!", 201)
-        else:
-            return make_response(jsonify("tous les cites sont : ", Ls), 200)
