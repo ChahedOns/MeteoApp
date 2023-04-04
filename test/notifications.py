b0fab3b7e36d5df755a2302a9dd3959efcb23b75
ChahedOns <chahedons1@gmail.com>
2023-04-04 04:34:37 +0100

    Working on kafka

--- test/notifications.py
+++ test/notifications.py
@@ -1,5 +1,7 @@
 from flask import Flask, Blueprint, session, make_response, request, jsonify
 from config import db_name, user_pwd,user_db
+from threading import Thread
+from confluent_kafka import Producer, Consumer
 from flask_mongoengine import MongoEngine
 from werkzeug.security import generate_password_hash ,check_password_hash
 from flask_login import current_user, login_required, login_user, logout_user, UserMixin,LoginManager
@@ -35,6 +37,43 @@ app.register_blueprint(routes_BP)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+#producer and consumer
+
+
+producer_config = {'bootstrap.servers':"pkc-6ojv2.us-west4.gcp.confluent.cloud:9092",
+        'security.protocol':'SASL_SSL',
+        'sasl.mechanisms':'PLAIN',
+        'sasl.username':'AMD3FYNELYF4W6FB',
+        'sasl.password':'OcUpeg7LE+26okMZJzCCLK319i7cDwJKDX8mw21BubKm70GU9XFRPI829mfI42eI'
+          }
+consumer_config = {'bootstrap.servers':"pkc-6ojv2.us-west4.gcp.confluent.cloud:9092",
+        'security.protocol':'SASL_SSL',
+        'sasl.mechanisms':'PLAIN',
+        'sasl.username':'AMD3FYNELYF4W6FB',
+        'sasl.password':'OcUpeg7LE+26okMZJzCCLK319i7cDwJKDX8mw21BubKm70GU9XFRPI829mfI42eI',
+        'auto.offset.reset':'earliest',
+        'group.id':'notificationCNC',
+        'enable.auto.commit':'true',
+        'max.poll.interval.ms':'3000000',}
+notif_producer = Producer(producer_config)
+
+
+def load_notif(msg,id):
+    notif_consumer = Consumer(consumer_config)
+    notif_consumer.subscribe(['Notification'])
+    while True:
+        event =notif_consumer.poll(1.0)
+        if event is None:
+            pass
+        elif event.error():
+            print(f'Bummer - {event.error()}')
+        else:
+            notif = json.loads(event.value())
+            add_notif(msg)
+
+def add_notif(msg):
+    n = Notification(id=id,msg=msg)
+    return make_response("Notification sent!",200)
 
 # Needed functions
 
@@ -161,6 +200,10 @@ class History (db.Document):
             "City":self.city
         }
 
+class Notification(db.Document):
+    id=db.IntField()
+    msg=db.StringField()
+    date=db.DateField(default=datetime.datetime.now)
 
 # App Routers
 
@@ -262,7 +305,7 @@ def login():
     pwd = request.form.get("pwd")
 
     check_user = User.objects(mail=mail).first()
-    if not check_user:
+    if check_user== None:
         return make_response("Mail invalide", 201)
 
     # Vérifier que le mot de passe est correct
@@ -299,5 +342,34 @@ def profile():
 def hello():
     return"<h1>hello world<\h1>"
 
+msg=""
+@app.route("/notification",methods=["GET", "POST"])
+def active_notif(msg=msg):
+    if request.method == "GET":
+        data=get_weather_data(api_key,"london")
+        weatherStatus= data["weather"][0]["main"]
+        if weatherStatus == "snow":
+            msg = "Stay at home, drink something warm!"
+        elif weatherStatus == "rain" or weatherStatus=="shower rain" or weatherStatus=="thunderstorm":
+            msg = "Don't forget your umbrella! it may rains today!"
+        elif weatherStatus == "mist":
+            msg = "Becareful and drive slowly today!"
+        elif weatherStatus== "clouds":
+            msg="It may be a sad weather today! Be productive"
+        else:
+            pass
+        return load_notif(msg=msg,id=3)
+
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run()
+
+@app.before_first_request
+def launch_consumer():
+    t = Thread(target=load_notif)
+    t.start()
